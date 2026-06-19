a9f78fe821d5e8effe2e53d17968a6e2d3ff17f4
Tarun Kumar <tarunkumawat0477@gmail.com>
2026-06-19 17:43:53 +0530

    const accountId =3022 let accountemail = "xyz@gmail.com" var accountpass = "12345" accountcity ="Jaipur"
    // accountId = 2 // Changing value of const is not allowed
    accountemail ="cbhyc@gmail.com"
    accountpass ="49523"
    accountcity ="udaipur"
    console.log(accountId)
    /*
    We dont use var because of issue in block scope and functional scope.
    */
    console.table([accountId,accountemail,accountpass,accountcity])

---
+++ variable.js
@@ -0,0 +1,14 @@
+const accountId =3022
+let accountemail = "xyz@gmail.com"
+var accountpass = "12345"
+accountcity ="Jaipur"
+
+// accountId = 2 // Changing value of const is not allowed
+accountemail ="cbhyc@gmail.com"
+accountpass ="49523"
+accountcity ="udaipur"
+console.log(accountId)
+/*
+We dont use var because of issue in block scope and functional scope.
+*/
+console.table([accountId,accountemail,accountpass,accountcity])
