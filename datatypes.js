fb4d2b2cc809b6fdd3cf57c0dfc8b1b70f40d805
Tarun Kumar <tarunkumawat0477@gmail.com>
2026-06-19 18:03:24 +0530

    let name ="Tarun" let age =19 let isLoggedIn = true let state;
    //number => 2 to power 53
    //bigint
    //string =>""
    //boolean =>true/false
    //null =>special type
    //undefined = You didnt define the value
    //symbol =>unique thing
    //object
    console.log(typeof null)//object
    console.log(typeof undefined)//undefined

---
+++ datatypes.js
@@ -0,0 +1,16 @@
+let name ="Tarun"
+let age =19
+let isLoggedIn = true
+let state;
+
+
+//number => 2 to power 53 
+//bigint 
+//string =>""
+//boolean =>true/false
+//null =>special type
+//undefined = You didnt define the value
+//symbol =>unique thing
+//object
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
