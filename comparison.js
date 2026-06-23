9b8283564c299c7b532391727d1f660c44dc74ba
Tarun Kumar <tarunkumawat0477@gmail.com>
2026-06-23 23:57:14 +0530

    let score = "33abc"; console.log(typeof score);
    console.log(typeof score);
    
    let valueInNumber = Number(score);
    console.log(typeof valueInNumber);
    
    console.log(valueInNumber);
    // "33"=>33
    // "33abc"=>NaN
    // true=>1;false=>0
    
    let isloggedin = "tarun";
    
    let booleanIsloggedIn = Boolean(isloggedin);
    console.log(booleanIsloggedIn);
    // 1=>true;0=>false
    // "" =>false
    // "hitesh"=>true
    let someNumber =33
    let stringNumber = String(someNumber);
    console.log(typeof stringNumber);
    // *********************** Operartions **********************//
    let value =3
    let negValue = -value
    // console.log(negValue)
    let str1 ="hello"
    let str2 =" tarun"
    let str3 = str1+str2
    console.log(str3);     //hello tarun
    
    console.log("1"+2);    // 12
    console.log(1+"2");    // 12
    console.log("1"+2+2);  // 122
    console.log(1+2+"2");  // 32
    
    console.log(str1+str2) // hello tarun

---
+++ comparison.js
@@ -0,0 +1,21 @@
+// console.log(2>1)    // true
+// console.log(2>=1)   // true
+// console.log(2<1)    // false
+// console.log(2==1)   // false
+// console.log(2!=1)   // true
+
+//************************************************** Comparison of different datatypes**************************************************//
+// console.log("2">1)   // true reason: javascript automatically converted it to number
+// console.log("hi">1)  // false reason: different datatype
+// console.log("02">1)  // true reason: javascript automatically converted it to number
+
+
+// console.log(null>0)  // false
+// console.log(null>=0) // true
+// console.log(null==0) // false
+// console.log(null!=0) // true
+
+console.log(undefined>0)  // false
+console.log(undefined>=0) // false
+console.log(undefined==0) // false
+console.log(undefined!=0) // true
--- conversion.js
+++ conversion.js
@@ -28,10 +28,12 @@ let negValue = -value
 let str1 ="hello"
 let str2 =" tarun"
 let str3 = str1+str2
-console.log(str3);
+console.log(str3);     //hello tarun
 
 console.log("1"+2);    // 12
 console.log(1+"2");    // 12
 console.log("1"+2+2);  // 122
 console.log(1+2+"2");  // 32
+
+console.log(str1+str2) // hello tarun
 
